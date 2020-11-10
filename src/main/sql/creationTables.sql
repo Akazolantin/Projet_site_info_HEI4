c11a2e89b6006338e86e8994326f18906a70268c
Martin de Foresta <martin.de-foresta@hei.yncrea.fr>
2020-11-10 10:49:35 +0100

    create creationTables

--- src/main/sql/creationTables.sql
+++ src/main/sql/creationTables.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `eleve` (
   `eleve_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
-  PRIMARY KEY (`genre_id`)
+  PRIMARY KEY (`eleve_id`)
 );
 
 CREATE TABLE `tache` (
-  `film_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tache_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(45) NOT NULL,
   `release_date` date NOT NULL,
-  `genre_id` int(11) NOT NULL,
+  `eleve_id` int(11) NOT NULL,
   `duration` int(11) NOT NULL,
   `director` varchar(45) NOT NULL,
   `summary` varchar(5000) DEFAULT NULL,
