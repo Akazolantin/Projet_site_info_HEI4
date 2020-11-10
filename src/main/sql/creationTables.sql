de9a63baf5c836fc4c4e43464809acd2fbbbf9da
Martin de Foresta <martin.de-foresta@hei.yncrea.fr>
2020-11-10 10:35:19 +0100

    create table sql creationTables, populateTables

--- src/main/sql/creationTables.sql
+++ src/main/sql/creationTables.sql
@@ -1,10 +1,10 @@
-CREATE TABLE `genre` (
-  `genre_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `eleve` (
+  `eleve_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
   PRIMARY KEY (`genre_id`)
 );
 
-CREATE TABLE `film` (
+CREATE TABLE `tache` (
   `film_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(45) NOT NULL,
   `release_date` date NOT NULL,
@@ -15,5 +15,9 @@ CREATE TABLE `film` (
   PRIMARY KEY (`film_id`),
   KEY `genre_id_fk` (`genre_id`),
   CONSTRAINT `genre_id_fk` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`genre_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+CREATE TABLE `identifiant` (
+
 );
 
